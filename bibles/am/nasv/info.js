3488df6b066a160f41d315c26454e3f511a0ad2d
VitalikL <lim.vitaliy@gmail.com>
2020-01-27 14:09:02 -0500

    Adding am/nasv

---
+++ bibles/am/nasv/info.js
@@ -0,0 +1,469 @@
+var info = {
+	"lang": "am",
+	"version": "nasv",
+	"books": [
+		{
+			"name": "ዘፍጥረት",
+			"synonyms": [
+				"Gen"
+			],
+			"numChapters": 50
+		},
+		{
+			"name": "ዘፀአት",
+			"synonyms": [
+				"Exod"
+			],
+			"numChapters": 40
+		},
+		{
+			"name": "ዘሌዋውያን",
+			"synonyms": [
+				"Lev"
+			],
+			"numChapters": 27
+		},
+		{
+			"name": "ዘኁልቍ",
+			"synonyms": [
+				"Num"
+			],
+			"numChapters": 36
+		},
+		{
+			"name": "ዘዳግም",
+			"synonyms": [
+				"Deut"
+			],
+			"numChapters": 34
+		},
+		{
+			"name": "ኢያሱ",
+			"synonyms": [
+				"Josh"
+			],
+			"numChapters": 24
+		},
+		{
+			"name": "መሳፍንት",
+			"synonyms": [
+				"Judg"
+			],
+			"numChapters": 21
+		},
+		{
+			"name": "ሩት",
+			"synonyms": [
+				"Ruth"
+			],
+			"numChapters": 4
+		},
+		{
+			"name": "1 ሳሙኤል",
+			"synonyms": [
+				"1Sam"
+			],
+			"numChapters": 31
+		},
+		{
+			"name": "2 ሳሙኤል",
+			"synonyms": [
+				"2Sam"
+			],
+			"numChapters": 24
+		},
+		{
+			"name": "1 ነገሥት",
+			"synonyms": [
+				"1Kgs"
+			],
+			"numChapters": 22
+		},
+		{
+			"name": "2 ነገሥት",
+			"synonyms": [
+				"2Kgs"
+			],
+			"numChapters": 25
+		},
+		{
+			"name": "1 ዜና መዋዕል",
+			"synonyms": [
+				"1Chr"
+			],
+			"numChapters": 29
+		},
+		{
+			"name": "2 ዜና መዋዕል",
+			"synonyms": [
+				"2Chr"
+			],
+			"numChapters": 36
+		},
+		{
+			"name": "ዕዝራ",
+			"synonyms": [
+				"Ezra"
+			],
+			"numChapters": 10
+		},
+		{
+			"name": "ነህምያ",
+			"synonyms": [
+				"Neh"
+			],
+			"numChapters": 13
+		},
+		{
+			"name": "አስቴር",
+			"synonyms": [
+				"Esth"
+			],
+			"numChapters": 10
+		},
+		{
+			"name": "ኢዮብ",
+			"synonyms": [
+				"Job"
+			],
+			"numChapters": 42
+		},
+		{
+			"name": "መዝሙር",
+			"synonyms": [
+				"Ps"
+			],
+			"numChapters": 150
+		},
+		{
+			"name": "ምሳሌ",
+			"synonyms": [
+				"Prov"
+			],
+			"numChapters": 31
+		},
+		{
+			"name": "መክብብ",
+			"synonyms": [
+				"Eccl"
+			],
+			"numChapters": 12
+		},
+		{
+			"name": "ማሕልየ መሓልይ",
+			"synonyms": [
+				"Song"
+			],
+			"numChapters": 8
+		},
+		{
+			"name": "ኢሳይያስ",
+			"synonyms": [
+				"Isa"
+			],
+			"numChapters": 66
+		},
+		{
+			"name": "ኤርምያስ",
+			"synonyms": [
+				"Jer"
+			],
+			"numChapters": 52
+		},
+		{
+			"name": "ሰቆቃወ",
+			"synonyms": [
+				"Lam"
+			],
+			"numChapters": 5
+		},
+		{
+			"name": "ሕዝቅኤል",
+			"synonyms": [
+				"Ezek"
+			],
+			"numChapters": 48
+		},
+		{
+			"name": "ዳንኤል",
+			"synonyms": [
+				"Dan"
+			],
+			"numChapters": 12
+		},
+		{
+			"name": "ሆሴዕ",
+			"synonyms": [
+				"Hos"
+			],
+			"numChapters": 14
+		},
+		{
+			"name": "ኢዩኤል",
+			"synonyms": [
+				"Joel"
+			],
+			"numChapters": 3
+		},
+		{
+			"name": "አሞጽ",
+			"synonyms": [
+				"Amos"
+			],
+			"numChapters": 9
+		},
+		{
+			"name": "አብድዩ",
+			"synonyms": [
+				"Obad"
+			],
+			"numChapters": 1
+		},
+		{
+			"name": "ዮናስ",
+			"synonyms": [
+				"Jonah"
+			],
+			"numChapters": 4
+		},
+		{
+			"name": "ሚክያስ",
+			"synonyms": [
+				"Mic"
+			],
+			"numChapters": 7
+		},
+		{
+			"name": "ናሆም",
+			"synonyms": [
+				"Nah"
+			],
+			"numChapters": 3
+		},
+		{
+			"name": "ዕንባቆም",
+			"synonyms": [
+				"Hab"
+			],
+			"numChapters": 3
+		},
+		{
+			"name": "ሶፎንያስ",
+			"synonyms": [
+				"Zeph"
+			],
+			"numChapters": 3
+		},
+		{
+			"name": "ሐጌ",
+			"synonyms": [
+				"Hag"
+			],
+			"numChapters": 2
+		},
+		{
+			"name": "ዘካርያስ",
+			"synonyms": [
+				"Zech"
+			],
+			"numChapters": 14
+		},
+		{
+			"name": "ሚልክያስ",
+			"synonyms": [
+				"Mal"
+			],
+			"numChapters": 4
+		},
+		{
+			"name": "ማቴዎስ",
+			"synonyms": [
+				"Matt"
+			],
+			"numChapters": 28
+		},
+		{
+			"name": "ማርቆስ",
+			"synonyms": [
+				"Mark"
+			],
+			"numChapters": 16
+		},
+		{
+			"name": "ሉቃስ",
+			"synonyms": [
+				"Luke"
+			],
+			"numChapters": 24
+		},
+		{
+			"name": "ዮሐንስ",
+			"synonyms": [
+				"John"
+			],
+			"numChapters": 21
+		},
+		{
+			"name": "ሐዋርያት ሥራ",
+			"synonyms": [
+				"Acts"
+			],
+			"numChapters": 28
+		},
+		{
+			"name": "ሮሜ",
+			"synonyms": [
+				"Rom"
+			],
+			"numChapters": 16
+		},
+		{
+			"name": "1 ቆሮንቶስ",
+			"synonyms": [
+				"1Cor"
+			],
+			"numChapters": 16
+		},
+		{
+			"name": "2 ቆሮንቶስ",
+			"synonyms": [
+				"2Cor"
+			],
+			"numChapters": 13
+		},
+		{
+			"name": "ገላትያ",
+			"synonyms": [
+				"Gal"
+			],
+			"numChapters": 6
+		},
+		{
+			"name": "ኤፌሶን",
+			"synonyms": [
+				"Eph"
+			],
+			"numChapters": 6
+		},
+		{
+			"name": "ፊልጵስዩስ",
+			"synonyms": [
+				"Phil"
+			],
+			"numChapters": 4
+		},
+		{
+			"name": "ቈላስይስ",
+			"synonyms": [
+				"Col"
+			],
+			"numChapters": 4
+		},
+		{
+			"name": "1 ተሰሎንቄ",
+			"synonyms": [
+				"1Thess"
+			],
+			"numChapters": 5
+		},
+		{
+			"name": "2 ተሰሎንቄ",
+			"synonyms": [
+				"2Thess"
+			],
+			"numChapters": 3
+		},
+		{
+			"name": "1 ጢሞቴዎስ",
+			"synonyms": [
+				"1Tim"
+			],
+			"numChapters": 6
+		},
+		{
+			"name": "2 ጢሞቴዎስ",
+			"synonyms": [
+				"2Tim"
+			],
+			"numChapters": 4
+		},
+		{
+			"name": "ቲቶ",
+			"synonyms": [
+				"Titus"
+			],
+			"numChapters": 3
+		},
+		{
+			"name": "ፊልሞና",
+			"synonyms": [
+				"Phlm"
+			],
+			"numChapters": 1
+		},
+		{
+			"name": "ዕብራውያን",
+			"synonyms": [
+				"Heb"
+			],
+			"numChapters": 13
+		},
+		{
+			"name": "ያዕቆብ",
+			"synonyms": [
+				"Jas"
+			],
+			"numChapters": 5
+		},
+		{
+			"name": "1 ጴጥሮስ",
+			"synonyms": [
+				"1Pet"
+			],
+			"numChapters": 5
+		},
+		{
+			"name": "2 ጴጥሮስ",
+			"synonyms": [
+				"2Pet"
+			],
+			"numChapters": 3
+		},
+		{
+			"name": "1 ዮሐንስ",
+			"synonyms": [
+				"1John"
+			],
+			"numChapters": 5
+		},
+		{
+			"name": "2 ዮሐንስ",
+			"synonyms": [
+				"2John"
+			],
+			"numChapters": 1
+		},
+		{
+			"name": "3 ዮሐንስ",
+			"synonyms": [
+				"3John"
+			],
+			"numChapters": 1
+		},
+		{
+			"name": "ይሁዳ",
+			"synonyms": [
+				"Jude"
+			],
+			"numChapters": 1
+		},
+		{
+			"name": "ራእይ",
+			"synonyms": [
+				"Rev"
+			],
+			"numChapters": 22
+		}
+	]
+};
+module.exports = info;
