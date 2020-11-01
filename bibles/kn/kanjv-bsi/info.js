fc887b8871b7d9349367e5334dbc92a3738b70a3
VitalikL <lim.vitaliy@gmail.com>
2020-11-01 14:26:51 -0500

    Add kn/kanjv-bsi

---
+++ bibles/kn/kanjv-bsi/info.js
@@ -0,0 +1,469 @@
+var info = {
+	"lang": "kn",
+	"version": "kanjv-bsi",
+	"books": [
+		{
+			"name": "ಆದಿಕಾಂಡ",
+			"synonyms": [
+				"Gen"
+			],
+			"numChapters": 50
+		},
+		{
+			"name": "ವಿಮೋಚನಕಾಂಡ",
+			"synonyms": [
+				"Exod"
+			],
+			"numChapters": 40
+		},
+		{
+			"name": "ಯಾಜಕಕಾಂಡ",
+			"synonyms": [
+				"Lev"
+			],
+			"numChapters": 27
+		},
+		{
+			"name": "ಅರಣ್ಯಕಾಂಡ",
+			"synonyms": [
+				"Num"
+			],
+			"numChapters": 36
+		},
+		{
+			"name": "ಧರ್ಮೋಪದೇಶಕಾಂಡ",
+			"synonyms": [
+				"Deut"
+			],
+			"numChapters": 34
+		},
+		{
+			"name": "ಯೆಹೋಶುವ",
+			"synonyms": [
+				"Josh"
+			],
+			"numChapters": 24
+		},
+		{
+			"name": "ನ್ಯಾಯಸ್ಥಾಪಕರು",
+			"synonyms": [
+				"Judg"
+			],
+			"numChapters": 21
+		},
+		{
+			"name": "ರೂತಳು",
+			"synonyms": [
+				"Ruth"
+			],
+			"numChapters": 4
+		},
+		{
+			"name": "1 ಸಮುವೇಲನು",
+			"synonyms": [
+				"1Sam"
+			],
+			"numChapters": 31
+		},
+		{
+			"name": "2 ಸಮುವೇಲನು",
+			"synonyms": [
+				"2Sam"
+			],
+			"numChapters": 24
+		},
+		{
+			"name": "1 ಅರಸುಗಳು",
+			"synonyms": [
+				"1Kgs"
+			],
+			"numChapters": 22
+		},
+		{
+			"name": "2 ಅರಸುಗಳು",
+			"synonyms": [
+				"2Kgs"
+			],
+			"numChapters": 25
+		},
+		{
+			"name": "1 ಪೂರ್ವಕಾಲವೃತ್ತಾಂತ",
+			"synonyms": [
+				"1Chr"
+			],
+			"numChapters": 29
+		},
+		{
+			"name": "2 ಪೂರ್ವಕಾಲವೃತ್ತಾಂತ",
+			"synonyms": [
+				"2Chr"
+			],
+			"numChapters": 36
+		},
+		{
+			"name": "ಎಜ್ರನು",
+			"synonyms": [
+				"Ezra"
+			],
+			"numChapters": 10
+		},
+		{
+			"name": "ನೆಹೆಮೀಯ",
+			"synonyms": [
+				"Neh"
+			],
+			"numChapters": 13
+		},
+		{
+			"name": "ಎಸ್ತೇರಳು",
+			"synonyms": [
+				"Esth"
+			],
+			"numChapters": 10
+		},
+		{
+			"name": "ಯೋಬನು",
+			"synonyms": [
+				"Job"
+			],
+			"numChapters": 42
+		},
+		{
+			"name": "ಕೀರ್ತನೆಗಳು",
+			"synonyms": [
+				"Ps"
+			],
+			"numChapters": 150
+		},
+		{
+			"name": "ಜ್ಞಾನೋಕ್ತಿಗಳು",
+			"synonyms": [
+				"Prov"
+			],
+			"numChapters": 31
+		},
+		{
+			"name": "ಪ್ರಸಂಗಿ",
+			"synonyms": [
+				"Eccl"
+			],
+			"numChapters": 12
+		},
+		{
+			"name": "ಪರಮಗೀತ",
+			"synonyms": [
+				"Song"
+			],
+			"numChapters": 8
+		},
+		{
+			"name": "ಯೆಶಾಯ",
+			"synonyms": [
+				"Isa"
+			],
+			"numChapters": 66
+		},
+		{
+			"name": "ಯೆರೆಮೀಯ",
+			"synonyms": [
+				"Jer"
+			],
+			"numChapters": 52
+		},
+		{
+			"name": "ಪ್ರಲಾಪಗಳು",
+			"synonyms": [
+				"Lam"
+			],
+			"numChapters": 5
+		},
+		{
+			"name": "ಯೆಹೆಜ್ಕೇಲ",
+			"synonyms": [
+				"Ezek"
+			],
+			"numChapters": 48
+		},
+		{
+			"name": "ದಾನಿಯೇಲನು",
+			"synonyms": [
+				"Dan"
+			],
+			"numChapters": 12
+		},
+		{
+			"name": "ಹೋಶೇಯ",
+			"synonyms": [
+				"Hos"
+			],
+			"numChapters": 14
+		},
+		{
+			"name": "ಯೋವೇಲ",
+			"synonyms": [
+				"Joel"
+			],
+			"numChapters": 3
+		},
+		{
+			"name": "ಆಮೋಸ",
+			"synonyms": [
+				"Amos"
+			],
+			"numChapters": 9
+		},
+		{
+			"name": "ಓಬದ್ಯ",
+			"synonyms": [
+				"Obad"
+			],
+			"numChapters": 1
+		},
+		{
+			"name": "ಯೋನ",
+			"synonyms": [
+				"Jonah"
+			],
+			"numChapters": 4
+		},
+		{
+			"name": "ಮೀಕ",
+			"synonyms": [
+				"Mic"
+			],
+			"numChapters": 7
+		},
+		{
+			"name": "ನಹೂಮ",
+			"synonyms": [
+				"Nah"
+			],
+			"numChapters": 3
+		},
+		{
+			"name": "ಹಬಕ್ಕೂಕ",
+			"synonyms": [
+				"Hab"
+			],
+			"numChapters": 3
+		},
+		{
+			"name": "ಚೆಫನ್ಯ",
+			"synonyms": [
+				"Zeph"
+			],
+			"numChapters": 3
+		},
+		{
+			"name": "ಹಗ್ಗಾಯ",
+			"synonyms": [
+				"Hag"
+			],
+			"numChapters": 2
+		},
+		{
+			"name": "ಜೆಕರ್ಯ",
+			"synonyms": [
+				"Zech"
+			],
+			"numChapters": 14
+		},
+		{
+			"name": "ಮಲಾಕಿಯ",
+			"synonyms": [
+				"Mal"
+			],
+			"numChapters": 4
+		},
+		{
+			"name": "ಮತ್ತಾಯ",
+			"synonyms": [
+				"Matt"
+			],
+			"numChapters": 28
+		},
+		{
+			"name": "ಮಾರ್ಕ",
+			"synonyms": [
+				"Mark"
+			],
+			"numChapters": 16
+		},
+		{
+			"name": "ಲೂಕ",
+			"synonyms": [
+				"Luke"
+			],
+			"numChapters": 24
+		},
+		{
+			"name": "ಯೋಹಾನ",
+			"synonyms": [
+				"John"
+			],
+			"numChapters": 21
+		},
+		{
+			"name": "ಅಪೊಸ್ತಲರ ಕೃತ್ಯಗಳು",
+			"synonyms": [
+				"Acts"
+			],
+			"numChapters": 28
+		},
+		{
+			"name": "ರೋಮಾಪುರದವರಿಗೆ",
+			"synonyms": [
+				"Rom"
+			],
+			"numChapters": 16
+		},
+		{
+			"name": "1 ಕೊರಿಂಥದವರಿಗೆ",
+			"synonyms": [
+				"1Cor"
+			],
+			"numChapters": 16
+		},
+		{
+			"name": "2 ಕೊರಿಂಥದವರಿಗೆ",
+			"synonyms": [
+				"2Cor"
+			],
+			"numChapters": 13
+		},
+		{
+			"name": "ಗಲಾತ್ಯದವರಿಗೆ",
+			"synonyms": [
+				"Gal"
+			],
+			"numChapters": 6
+		},
+		{
+			"name": "ಎಫೆಸದವರಿಗೆ",
+			"synonyms": [
+				"Eph"
+			],
+			"numChapters": 6
+		},
+		{
+			"name": "ಫಿಲಿಪ್ಪಿಯವರಿಗೆ",
+			"synonyms": [
+				"Phil"
+			],
+			"numChapters": 4
+		},
+		{
+			"name": "ಕೊಲೊಸ್ಸೆಯವರಿಗೆ",
+			"synonyms": [
+				"Col"
+			],
+			"numChapters": 4
+		},
+		{
+			"name": "1 ಥೆಸಲೋನಿಕದವರಿಗೆ",
+			"synonyms": [
+				"1Thess"
+			],
+			"numChapters": 5
+		},
+		{
+			"name": "2 ಥೆಸಲೋನಿಕದವರಿಗೆ",
+			"synonyms": [
+				"2Thess"
+			],
+			"numChapters": 3
+		},
+		{
+			"name": "1 ತಿಮೊಥೆಯನಿಗೆ",
+			"synonyms": [
+				"1Tim"
+			],
+			"numChapters": 6
+		},
+		{
+			"name": "2 ತಿಮೊಥೆಯನಿಗೆ",
+			"synonyms": [
+				"2Tim"
+			],
+			"numChapters": 4
+		},
+		{
+			"name": "ತೀತನಿಗೆ",
+			"synonyms": [
+				"Titus"
+			],
+			"numChapters": 3
+		},
+		{
+			"name": "ಫಿಲೆಮೋನನಿಗೆ",
+			"synonyms": [
+				"Phlm"
+			],
+			"numChapters": 1
+		},
+		{
+			"name": "ಇಬ್ರಿಯರಿಗೆ",
+			"synonyms": [
+				"Heb"
+			],
+			"numChapters": 13
+		},
+		{
+			"name": "ಯಾಕೋಬನು",
+			"synonyms": [
+				"Jas"
+			],
+			"numChapters": 5
+		},
+		{
+			"name": "1 ಪೇತ್ರನು",
+			"synonyms": [
+				"1Pet"
+			],
+			"numChapters": 5
+		},
+		{
+			"name": "2 ಪೇತ್ರನು",
+			"synonyms": [
+				"2Pet"
+			],
+			"numChapters": 3
+		},
+		{
+			"name": "1 ಯೋಹಾನನು",
+			"synonyms": [
+				"1John"
+			],
+			"numChapters": 5
+		},
+		{
+			"name": "2 ಯೋಹಾನನು",
+			"synonyms": [
+				"2John"
+			],
+			"numChapters": 1
+		},
+		{
+			"name": "3 ಯೋಹಾನನು",
+			"synonyms": [
+				"3John"
+			],
+			"numChapters": 1
+		},
+		{
+			"name": "ಯೂದನು",
+			"synonyms": [
+				"Jude"
+			],
+			"numChapters": 1
+		},
+		{
+			"name": "ಪ್ರಕಟನೆ",
+			"synonyms": [
+				"Rev"
+			],
+			"numChapters": 22
+		}
+	]
+};
+module.exports = info;
