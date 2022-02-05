d905d041aac2588273ddae26d884a457ed9130aa
VitalikL <lim.vitaliy@gmail.com>
2022-02-04 21:02:12 -0500

    Adding Heb 1, replacing <span> with <sup>

--- bibles/et/eep/books/63.js
+++ bibles/et/eep/books/63.js
@@ -3,19 +3,19 @@ var book = {
 	"numChapters": 1,
 	"chapters": {
 		"1": {
-			"1": "<span>1</span> Vanem - valitud emandale ja tema lastele, keda ma armastan tões, ja mitte üksi mina, vaid kõik, kes on tunnetanud tõde,",
-			"2": "<span>2</span> tõe tõttu, mis püsib meis ja jääb meiega igavesti:",
-			"3": "<span>3</span> Olgu meiega arm, halastus, rahu Jumalalt Isalt ja Jeesuselt Kristuselt, Isa Pojalt, tões ja armastuses!",
-			"4": "<span>4</span> Rõõmustasin väga, et leidsin sinu laste seast neid, kes käivad tões, nii nagu me oleme Isa käest saanud käsu.",
-			"5": "<span>5</span> Ja nüüd ma palun sind, emand, mitte kui uut käsku sulle kirjutades, vaid seda, mis meil on olnud algusest, et me peame üksteist armastama.",
-			"6": "<span>6</span> Ja see ongi armastus, et me käime tema käskude järgi. See ongi käsk, nii nagu te olete kuulnud algusest, et te käiksite selles.",
-			"7": "<span>7</span> Jah, maailma on läinud välja palju eksitajaid. Kes ei tunnista Jeesust Kristust lihasse tulnuna, seesama ongi eksitaja ja antikristus.",
-			"8": "<span>8</span> Pange tähele iseennast, et te ei kaotaks seda, millega me oleme vaeva näinud, vaid et te saaksite kätte täie palga.",
-			"9": "<span>9</span> Ühelgi, kes astub üle Kristuse õpetusest ega püsi selles, ei ole Jumalat. Kes püsib Kristuse õpetuses, sellel on nii Isa kui ka Poeg.",
-			"10": "<span>10</span> Kui keegi tuleb teie juurde ega too seda õpetust, ärge võtke teda oma majja ja ärge öelge talle: 'Tere tulemast!'",
-			"11": "<span>11</span> Sest kes talle ütleb: 'Tere tulemast!', saab osalejaks ta kurjades tegudes.",
-			"12": "<span>12</span> Mul oleks teile palju kirjutada, aga ma ei taha seda teha paberil ja tindiga, vaid loodan ise tulla teie juurde ja rääkida silmast silma, et meie rõõm oleks täielik.",
-			"13": "<span>13</span> Sind tervitavad su valitud õe lapsed!"
+			"1": "<sup>1</sup> Vanem - valitud emandale ja tema lastele, keda ma armastan tões, ja mitte üksi mina, vaid kõik, kes on tunnetanud tõde,",
+			"2": "<sup>2</sup> tõe tõttu, mis püsib meis ja jääb meiega igavesti:",
+			"3": "<sup>3</sup> Olgu meiega arm, halastus, rahu Jumalalt Isalt ja Jeesuselt Kristuselt, Isa Pojalt, tões ja armastuses!",
+			"4": "<sup>4</sup> Rõõmustasin väga, et leidsin sinu laste seast neid, kes käivad tões, nii nagu me oleme Isa käest saanud käsu.",
+			"5": "<sup>5</sup> Ja nüüd ma palun sind, emand, mitte kui uut käsku sulle kirjutades, vaid seda, mis meil on olnud algusest, et me peame üksteist armastama.",
+			"6": "<sup>6</sup> Ja see ongi armastus, et me käime tema käskude järgi. See ongi käsk, nii nagu te olete kuulnud algusest, et te käiksite selles.",
+			"7": "<sup>7</sup> Jah, maailma on läinud välja palju eksitajaid. Kes ei tunnista Jeesust Kristust lihasse tulnuna, seesama ongi eksitaja ja antikristus.",
+			"8": "<sup>8</sup> Pange tähele iseennast, et te ei kaotaks seda, millega me oleme vaeva näinud, vaid et te saaksite kätte täie palga.",
+			"9": "<sup>9</sup> Ühelgi, kes astub üle Kristuse õpetusest ega püsi selles, ei ole Jumalat. Kes püsib Kristuse õpetuses, sellel on nii Isa kui ka Poeg.",
+			"10": "<sup>10</sup> Kui keegi tuleb teie juurde ega too seda õpetust, ärge võtke teda oma majja ja ärge öelge talle: 'Tere tulemast!'",
+			"11": "<sup>11</sup> Sest kes talle ütleb: 'Tere tulemast!', saab osalejaks ta kurjades tegudes.",
+			"12": "<sup>12</sup> Mul oleks teile palju kirjutada, aga ma ei taha seda teha paberil ja tindiga, vaid loodan ise tulla teie juurde ja rääkida silmast silma, et meie rõõm oleks täielik.",
+			"13": "<sup>13</sup> Sind tervitavad su valitud õe lapsed!"
 		}
 	}
 };
