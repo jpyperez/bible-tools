70b131d4b178ca568a2a3b2f441fab8275050699
VitalikL <lim.vitaliy@gmail.com>
2022-07-19 11:54:59 -0400

    Adding mr/mar

---
+++ bibles/mr/mar/info.js
@@ -0,0 +1,469 @@
+var info = {
+	"lang": "mr",
+	"version": "mar",
+	"books": [
+		{
+			"name": "उत्पत्ती",
+			"numChapters": 50,
+			"synonyms": [
+				"Gen"
+			]
+		},
+		{
+			"name": "निर्गम",
+			"numChapters": 40,
+			"synonyms": [
+				"Exod"
+			]
+		},
+		{
+			"name": "लेवीय",
+			"numChapters": 27,
+			"synonyms": [
+				"Lev"
+			]
+		},
+		{
+			"name": "गणना",
+			"numChapters": 36,
+			"synonyms": [
+				"Num"
+			]
+		},
+		{
+			"name": "अनुवाद",
+			"numChapters": 34,
+			"synonyms": [
+				"Deut"
+			]
+		},
+		{
+			"name": "यहोशवा",
+			"numChapters": 24,
+			"synonyms": [
+				"Josh"
+			]
+		},
+		{
+			"name": "शास्ते",
+			"numChapters": 21,
+			"synonyms": [
+				"Judg"
+			]
+		},
+		{
+			"name": "रूथ",
+			"numChapters": 4,
+			"synonyms": [
+				"Ruth"
+			]
+		},
+		{
+			"name": "१ शमुवेल",
+			"numChapters": 31,
+			"synonyms": [
+				"1Sam"
+			]
+		},
+		{
+			"name": "२ शमुवेल",
+			"numChapters": 24,
+			"synonyms": [
+				"2Sam"
+			]
+		},
+		{
+			"name": "१ राजे",
+			"numChapters": 22,
+			"synonyms": [
+				"1Kgs"
+			]
+		},
+		{
+			"name": "२ राजे",
+			"numChapters": 25,
+			"synonyms": [
+				"2Kgs"
+			]
+		},
+		{
+			"name": "१ इतिहास",
+			"numChapters": 29,
+			"synonyms": [
+				"1Chr"
+			]
+		},
+		{
+			"name": "२ इतिहास",
+			"numChapters": 36,
+			"synonyms": [
+				"2Chr"
+			]
+		},
+		{
+			"name": "एज्रा",
+			"numChapters": 10,
+			"synonyms": [
+				"Ezra"
+			]
+		},
+		{
+			"name": "नहेम्या",
+			"numChapters": 13,
+			"synonyms": [
+				"Neh"
+			]
+		},
+		{
+			"name": "एस्तेर",
+			"numChapters": 10,
+			"synonyms": [
+				"Esth"
+			]
+		},
+		{
+			"name": "ईयोब",
+			"numChapters": 42,
+			"synonyms": [
+				"Job"
+			]
+		},
+		{
+			"name": "स्तोत्रसंहिता",
+			"numChapters": 150,
+			"synonyms": [
+				"Ps"
+			]
+		},
+		{
+			"name": "नीतिसूत्रे",
+			"numChapters": 31,
+			"synonyms": [
+				"Prov"
+			]
+		},
+		{
+			"name": "उपदेशक",
+			"numChapters": 12,
+			"synonyms": [
+				"Eccl"
+			]
+		},
+		{
+			"name": "गीतरत्न",
+			"numChapters": 8,
+			"synonyms": [
+				"Song"
+			]
+		},
+		{
+			"name": "यशया",
+			"numChapters": 66,
+			"synonyms": [
+				"Isa"
+			]
+		},
+		{
+			"name": "यिर्मया",
+			"numChapters": 52,
+			"synonyms": [
+				"Jer"
+			]
+		},
+		{
+			"name": "विलापगीत",
+			"numChapters": 5,
+			"synonyms": [
+				"Lam"
+			]
+		},
+		{
+			"name": "यहेज्केल",
+			"numChapters": 48,
+			"synonyms": [
+				"Ezek"
+			]
+		},
+		{
+			"name": "दानीएल",
+			"numChapters": 12,
+			"synonyms": [
+				"Dan"
+			]
+		},
+		{
+			"name": "होशेय",
+			"numChapters": 14,
+			"synonyms": [
+				"Hos"
+			]
+		},
+		{
+			"name": "योएल",
+			"numChapters": 3,
+			"synonyms": [
+				"Joel"
+			]
+		},
+		{
+			"name": "आमोस",
+			"numChapters": 9,
+			"synonyms": [
+				"Amos"
+			]
+		},
+		{
+			"name": "ओबद्या",
+			"numChapters": 1,
+			"synonyms": [
+				"Obad"
+			]
+		},
+		{
+			"name": "योना",
+			"numChapters": 4,
+			"synonyms": [
+				"Jonah"
+			]
+		},
+		{
+			"name": "मीखा",
+			"numChapters": 7,
+			"synonyms": [
+				"Mic"
+			]
+		},
+		{
+			"name": "नहूम",
+			"numChapters": 3,
+			"synonyms": [
+				"Nah"
+			]
+		},
+		{
+			"name": "हबक्कूक",
+			"numChapters": 3,
+			"synonyms": [
+				"Hab"
+			]
+		},
+		{
+			"name": "सफन्या",
+			"numChapters": 3,
+			"synonyms": [
+				"Zeph"
+			]
+		},
+		{
+			"name": "हाग्गय",
+			"numChapters": 2,
+			"synonyms": [
+				"Hag"
+			]
+		},
+		{
+			"name": "जखर्‍या",
+			"numChapters": 14,
+			"synonyms": [
+				"Zech"
+			]
+		},
+		{
+			"name": "मलाखी",
+			"numChapters": 4,
+			"synonyms": [
+				"Mal"
+			]
+		},
+		{
+			"name": "मत्तय",
+			"numChapters": 28,
+			"synonyms": [
+				"Matt"
+			]
+		},
+		{
+			"name": "मार्क",
+			"numChapters": 16,
+			"synonyms": [
+				"Mark"
+			]
+		},
+		{
+			"name": "लूक",
+			"numChapters": 24,
+			"synonyms": [
+				"Luke"
+			]
+		},
+		{
+			"name": "योहान",
+			"numChapters": 21,
+			"synonyms": [
+				"John"
+			]
+		},
+		{
+			"name": "प्रेषितांची कृत्ये",
+			"numChapters": 28,
+			"synonyms": [
+				"Acts"
+			]
+		},
+		{
+			"name": "रोमकरांस पत्र",
+			"numChapters": 16,
+			"synonyms": [
+				"Rom"
+			]
+		},
+		{
+			"name": "१ करिंथ",
+			"numChapters": 16,
+			"synonyms": [
+				"1Cor"
+			]
+		},
+		{
+			"name": "२ करिंथ",
+			"numChapters": 13,
+			"synonyms": [
+				"2Cor"
+			]
+		},
+		{
+			"name": "गलतीकरांस पत्र",
+			"numChapters": 6,
+			"synonyms": [
+				"Gal"
+			]
+		},
+		{
+			"name": "इफिसकरांस पत्र",
+			"numChapters": 6,
+			"synonyms": [
+				"Eph"
+			]
+		},
+		{
+			"name": "फिलिप्पैकरांस पत्र",
+			"numChapters": 4,
+			"synonyms": [
+				"Phil"
+			]
+		},
+		{
+			"name": "कलस्सैकरांना पत्र",
+			"numChapters": 4,
+			"synonyms": [
+				"Col"
+			]
+		},
+		{
+			"name": "1 थेस्सल",
+			"numChapters": 5,
+			"synonyms": [
+				"1Thess"
+			]
+		},
+		{
+			"name": "2 थेस्सल",
+			"numChapters": 3,
+			"synonyms": [
+				"2Thess"
+			]
+		},
+		{
+			"name": "1 तीमथ्य",
+			"numChapters": 6,
+			"synonyms": [
+				"1Tim"
+			]
+		},
+		{
+			"name": "2 तीमथ्य",
+			"numChapters": 4,
+			"synonyms": [
+				"2Tim"
+			]
+		},
+		{
+			"name": "तीत",
+			"numChapters": 3,
+			"synonyms": [
+				"Titus"
+			]
+		},
+		{
+			"name": "फिलेमोन",
+			"numChapters": 1,
+			"synonyms": [
+				"Phlm"
+			]
+		},
+		{
+			"name": "इब्री",
+			"numChapters": 13,
+			"synonyms": [
+				"Heb"
+			]
+		},
+		{
+			"name": "याकोब",
+			"numChapters": 5,
+			"synonyms": [
+				"Jas"
+			]
+		},
+		{
+			"name": "1 पेत्र",
+			"numChapters": 5,
+			"synonyms": [
+				"1Pet"
+			]
+		},
+		{
+			"name": "2 पेत्र",
+			"numChapters": 3,
+			"synonyms": [
+				"2Pet"
+			]
+		},
+		{
+			"name": "1 योहान",
+			"numChapters": 5,
+			"synonyms": [
+				"1John"
+			]
+		},
+		{
+			"name": "2 योहान",
+			"numChapters": 1,
+			"synonyms": [
+				"2John"
+			]
+		},
+		{
+			"name": "3 योहान",
+			"numChapters": 1,
+			"synonyms": [
+				"3John"
+			]
+		},
+		{
+			"name": "यहूदा",
+			"numChapters": 1,
+			"synonyms": [
+				"Jude"
+			]
+		},
+		{
+			"name": "प्रकटी",
+			"numChapters": 22,
+			"synonyms": [
+				"Rev"
+			]
+		}
+	]
+};
+module.exports = info;
