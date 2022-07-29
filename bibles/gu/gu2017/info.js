2b700d3e09bae1858123dc53167971a5cd9830f7
VitalikL <lim.vitaliy@gmail.com>
2022-07-29 12:08:12 -0400

    Adding gu/gu2017

---
+++ bibles/gu/gu2017/info.js
@@ -0,0 +1,469 @@
+var info = {
+	"lang": "gu",
+	"version": "gu2017",
+	"books": [
+		{
+			"name": "ઉત્પત્તિ",
+			"numChapters": 50,
+			"synonyms": [
+				"Gen"
+			]
+		},
+		{
+			"name": "નિર્ગમન",
+			"numChapters": 40,
+			"synonyms": [
+				"Exod"
+			]
+		},
+		{
+			"name": "લેવીય",
+			"numChapters": 27,
+			"synonyms": [
+				"Lev"
+			]
+		},
+		{
+			"name": "ગણના",
+			"numChapters": 36,
+			"synonyms": [
+				"Num"
+			]
+		},
+		{
+			"name": "પુનર્નિયમ",
+			"numChapters": 34,
+			"synonyms": [
+				"Deut"
+			]
+		},
+		{
+			"name": "યહોશુઆ",
+			"numChapters": 24,
+			"synonyms": [
+				"Josh"
+			]
+		},
+		{
+			"name": "ન્યાયાધીશો",
+			"numChapters": 21,
+			"synonyms": [
+				"Judg"
+			]
+		},
+		{
+			"name": "રૂથ",
+			"numChapters": 4,
+			"synonyms": [
+				"Ruth"
+			]
+		},
+		{
+			"name": "૧ શમુએલ",
+			"numChapters": 31,
+			"synonyms": [
+				"1Sam"
+			]
+		},
+		{
+			"name": "૨ શમુએલ",
+			"numChapters": 24,
+			"synonyms": [
+				"2Sam"
+			]
+		},
+		{
+			"name": "૧ રાજાઓ",
+			"numChapters": 22,
+			"synonyms": [
+				"1Kgs"
+			]
+		},
+		{
+			"name": "૨ રાજાઓ",
+			"numChapters": 25,
+			"synonyms": [
+				"2Kgs"
+			]
+		},
+		{
+			"name": "૧ કાળવૃત્તાંત",
+			"numChapters": 29,
+			"synonyms": [
+				"1Chr"
+			]
+		},
+		{
+			"name": "૨ કાળવૃત્તાંત",
+			"numChapters": 36,
+			"synonyms": [
+				"2Chr"
+			]
+		},
+		{
+			"name": "એઝરા",
+			"numChapters": 10,
+			"synonyms": [
+				"Ezra"
+			]
+		},
+		{
+			"name": "નહેમ્યા",
+			"numChapters": 13,
+			"synonyms": [
+				"Neh"
+			]
+		},
+		{
+			"name": "એસ્તેર",
+			"numChapters": 10,
+			"synonyms": [
+				"Esth"
+			]
+		},
+		{
+			"name": "અયૂબ",
+			"numChapters": 42,
+			"synonyms": [
+				"Job"
+			]
+		},
+		{
+			"name": "ગીતશાસ્‍ત્ર",
+			"numChapters": 150,
+			"synonyms": [
+				"Ps"
+			]
+		},
+		{
+			"name": "નીતિવચનો",
+			"numChapters": 31,
+			"synonyms": [
+				"Prov"
+			]
+		},
+		{
+			"name": "સભાશિક્ષક",
+			"numChapters": 12,
+			"synonyms": [
+				"Eccl"
+			]
+		},
+		{
+			"name": "ગીતોનું ગીત",
+			"numChapters": 8,
+			"synonyms": [
+				"Song"
+			]
+		},
+		{
+			"name": "યશાયા",
+			"numChapters": 66,
+			"synonyms": [
+				"Isa"
+			]
+		},
+		{
+			"name": "યર્મિયા",
+			"numChapters": 52,
+			"synonyms": [
+				"Jer"
+			]
+		},
+		{
+			"name": "યર્મિયાનો વિલાપ",
+			"numChapters": 5,
+			"synonyms": [
+				"Lam"
+			]
+		},
+		{
+			"name": "હઝકિયેલ",
+			"numChapters": 48,
+			"synonyms": [
+				"Ezek"
+			]
+		},
+		{
+			"name": "દાનિયેલ",
+			"numChapters": 12,
+			"synonyms": [
+				"Dan"
+			]
+		},
+		{
+			"name": "હોશિયા",
+			"numChapters": 14,
+			"synonyms": [
+				"Hos"
+			]
+		},
+		{
+			"name": "યોએલ",
+			"numChapters": 3,
+			"synonyms": [
+				"Joel"
+			]
+		},
+		{
+			"name": "આમોસ",
+			"numChapters": 9,
+			"synonyms": [
+				"Amos"
+			]
+		},
+		{
+			"name": "ઓબાદ્યા",
+			"numChapters": 1,
+			"synonyms": [
+				"Obad"
+			]
+		},
+		{
+			"name": "યૂના",
+			"numChapters": 4,
+			"synonyms": [
+				"Jonah"
+			]
+		},
+		{
+			"name": "મીખાહ",
+			"numChapters": 7,
+			"synonyms": [
+				"Mic"
+			]
+		},
+		{
+			"name": "નાહૂમ",
+			"numChapters": 3,
+			"synonyms": [
+				"Nah"
+			]
+		},
+		{
+			"name": "હબાકુક",
+			"numChapters": 3,
+			"synonyms": [
+				"Hab"
+			]
+		},
+		{
+			"name": "સફાન્યા",
+			"numChapters": 3,
+			"synonyms": [
+				"Zeph"
+			]
+		},
+		{
+			"name": "હાગ્ગાય",
+			"numChapters": 2,
+			"synonyms": [
+				"Hag"
+			]
+		},
+		{
+			"name": "ઝખાર્યા",
+			"numChapters": 14,
+			"synonyms": [
+				"Zech"
+			]
+		},
+		{
+			"name": "માલાખી",
+			"numChapters": 4,
+			"synonyms": [
+				"Mal"
+			]
+		},
+		{
+			"name": "માથ્થી",
+			"numChapters": 28,
+			"synonyms": [
+				"Matt"
+			]
+		},
+		{
+			"name": "માર્ક",
+			"numChapters": 16,
+			"synonyms": [
+				"Mark"
+			]
+		},
+		{
+			"name": "લૂક",
+			"numChapters": 24,
+			"synonyms": [
+				"Luke"
+			]
+		},
+		{
+			"name": "યોહાન",
+			"numChapters": 21,
+			"synonyms": [
+				"John"
+			]
+		},
+		{
+			"name": "પ્રેરિતોનાં કૃત્યો",
+			"numChapters": 28,
+			"synonyms": [
+				"Acts"
+			]
+		},
+		{
+			"name": "રોમનોને પત્ર",
+			"numChapters": 16,
+			"synonyms": [
+				"Rom"
+			]
+		},
+		{
+			"name": "કરિંથીઓને પહેલો પત્ર",
+			"numChapters": 16,
+			"synonyms": [
+				"1Cor"
+			]
+		},
+		{
+			"name": "કરિંથીઓને બીજો પત્ર",
+			"numChapters": 13,
+			"synonyms": [
+				"2Cor"
+			]
+		},
+		{
+			"name": "ગલાતીઓને પત્ર",
+			"numChapters": 6,
+			"synonyms": [
+				"Gal"
+			]
+		},
+		{
+			"name": "એફેસીઓને પત્ર",
+			"numChapters": 6,
+			"synonyms": [
+				"Eph"
+			]
+		},
+		{
+			"name": "ફિલિપીઓને પત્ર",
+			"numChapters": 4,
+			"synonyms": [
+				"Phil"
+			]
+		},
+		{
+			"name": "કલોસીઓને પત્ર",
+			"numChapters": 4,
+			"synonyms": [
+				"Col"
+			]
+		},
+		{
+			"name": "થેસ્સાલોનિકીઓને પહેલો પત્ર",
+			"numChapters": 5,
+			"synonyms": [
+				"1Thess"
+			]
+		},
+		{
+			"name": "થેસ્સાલોનિકીઓને બીજો પત્ર",
+			"numChapters": 3,
+			"synonyms": [
+				"2Thess"
+			]
+		},
+		{
+			"name": "તિમોથીને પહેલો પત્ર",
+			"numChapters": 6,
+			"synonyms": [
+				"1Tim"
+			]
+		},
+		{
+			"name": "તિમોથીને બીજો પત્ર",
+			"numChapters": 4,
+			"synonyms": [
+				"2Tim"
+			]
+		},
+		{
+			"name": "તિતસને પત્ર",
+			"numChapters": 3,
+			"synonyms": [
+				"Titus"
+			]
+		},
+		{
+			"name": "ફિલેમોનને પાઉલ પ્રેરિતનો પત્ર",
+			"numChapters": 1,
+			"synonyms": [
+				"Phlm"
+			]
+		},
+		{
+			"name": "હિબ્રૂઓને પત્ર",
+			"numChapters": 13,
+			"synonyms": [
+				"Heb"
+			]
+		},
+		{
+			"name": "યાકૂબનો પત્ર",
+			"numChapters": 5,
+			"synonyms": [
+				"Jas"
+			]
+		},
+		{
+			"name": "પિતરનો પહેલો પત્ર",
+			"numChapters": 5,
+			"synonyms": [
+				"1Pet"
+			]
+		},
+		{
+			"name": "પિતરનો બીજો પત્ર",
+			"numChapters": 3,
+			"synonyms": [
+				"2Pet"
+			]
+		},
+		{
+			"name": "યોહાનનો પહેલો પત્ર",
+			"numChapters": 5,
+			"synonyms": [
+				"1John"
+			]
+		},
+		{
+			"name": "યોહાનનો બીજો પત્ર",
+			"numChapters": 1,
+			"synonyms": [
+				"2John"
+			]
+		},
+		{
+			"name": "યોહાનનો ત્રીજો પત્ર",
+			"numChapters": 1,
+			"synonyms": [
+				"3John"
+			]
+		},
+		{
+			"name": "યહૂદાનો પત્ર",
+			"numChapters": 1,
+			"synonyms": [
+				"Jude"
+			]
+		},
+		{
+			"name": "પ્રકટીકરણ",
+			"numChapters": 22,
+			"synonyms": [
+				"Rev"
+			]
+		}
+	]
+};
+module.exports = info;
