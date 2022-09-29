675f21913f0314e960b78b9bc97932e23d270c1c
VitalikL <lim.vitaliy@gmail.com>
2022-09-29 15:34:21 -0400

    Adding bg/ribbd

---
+++ bibles/bg/ribbd/info.js
@@ -0,0 +1,469 @@
+var info = {
+	"lang": "bg",
+	"version": "ribbd",
+	"books": [
+		{
+			"name": "Битие",
+			"synonyms": [
+				"Gen"
+			],
+			"numChapters": 50
+		},
+		{
+			"name": "Изход",
+			"synonyms": [
+				"Exod"
+			],
+			"numChapters": 40
+		},
+		{
+			"name": "Левит",
+			"synonyms": [
+				"Lev"
+			],
+			"numChapters": 27
+		},
+		{
+			"name": "Числа",
+			"synonyms": [
+				"Num"
+			],
+			"numChapters": 36
+		},
+		{
+			"name": "Второзаконие",
+			"synonyms": [
+				"Deut"
+			],
+			"numChapters": 34
+		},
+		{
+			"name": "Исус Навин",
+			"synonyms": [
+				"Josh"
+			],
+			"numChapters": 24
+		},
+		{
+			"name": "Съдии",
+			"synonyms": [
+				"Judg"
+			],
+			"numChapters": 21
+		},
+		{
+			"name": "Рут",
+			"synonyms": [
+				"Ruth"
+			],
+			"numChapters": 4
+		},
+		{
+			"name": "1 Книга На Царете",
+			"synonyms": [
+				"1Sam"
+			],
+			"numChapters": 31
+		},
+		{
+			"name": "2 Книга На Царете",
+			"synonyms": [
+				"2Sam"
+			],
+			"numChapters": 24
+		},
+		{
+			"name": "3 Книга На Царете",
+			"synonyms": [
+				"1Kgs"
+			],
+			"numChapters": 22
+		},
+		{
+			"name": "4 Книга На Царете",
+			"synonyms": [
+				"2Kgs"
+			],
+			"numChapters": 25
+		},
+		{
+			"name": "1 Книга На Летописите",
+			"synonyms": [
+				"1Chr"
+			],
+			"numChapters": 29
+		},
+		{
+			"name": "2 Книга На Летописите",
+			"synonyms": [
+				"2Chr"
+			],
+			"numChapters": 36
+		},
+		{
+			"name": "Ездра",
+			"synonyms": [
+				"Ezra"
+			],
+			"numChapters": 10
+		},
+		{
+			"name": "Неемия",
+			"synonyms": [
+				"Neh"
+			],
+			"numChapters": 13
+		},
+		{
+			"name": "Естир",
+			"synonyms": [
+				"Esth"
+			],
+			"numChapters": 10
+		},
+		{
+			"name": "Йов",
+			"synonyms": [
+				"Job"
+			],
+			"numChapters": 42
+		},
+		{
+			"name": "Псалми",
+			"synonyms": [
+				"Ps"
+			],
+			"numChapters": 150
+		},
+		{
+			"name": "Притчи",
+			"synonyms": [
+				"Prov"
+			],
+			"numChapters": 31
+		},
+		{
+			"name": "Еклисиаст",
+			"synonyms": [
+				"Eccl"
+			],
+			"numChapters": 12
+		},
+		{
+			"name": "Песен На Песните",
+			"synonyms": [
+				"Song"
+			],
+			"numChapters": 8
+		},
+		{
+			"name": "Исая",
+			"synonyms": [
+				"Isa"
+			],
+			"numChapters": 66
+		},
+		{
+			"name": "Йеремия",
+			"synonyms": [
+				"Jer"
+			],
+			"numChapters": 52
+		},
+		{
+			"name": "Плачът На Йеремия",
+			"synonyms": [
+				"Lam"
+			],
+			"numChapters": 5
+		},
+		{
+			"name": "Йезекиил",
+			"synonyms": [
+				"Ezek"
+			],
+			"numChapters": 48
+		},
+		{
+			"name": "Даниил",
+			"synonyms": [
+				"Dan"
+			],
+			"numChapters": 12
+		},
+		{
+			"name": "Осия",
+			"synonyms": [
+				"Hos"
+			],
+			"numChapters": 14
+		},
+		{
+			"name": "Амос",
+			"synonyms": [
+				"Amos"
+			],
+			"numChapters": 9
+		},
+		{
+			"name": "Михей",
+			"synonyms": [
+				"Mic"
+			],
+			"numChapters": 7
+		},
+		{
+			"name": "Йоил",
+			"synonyms": [
+				"Joel"
+			],
+			"numChapters": 3
+		},
+		{
+			"name": "Авдий",
+			"synonyms": [
+				"Obad"
+			],
+			"numChapters": 1
+		},
+		{
+			"name": "Йона",
+			"synonyms": [
+				"Jonah"
+			],
+			"numChapters": 4
+		},
+		{
+			"name": "Наум",
+			"synonyms": [
+				"Nah"
+			],
+			"numChapters": 3
+		},
+		{
+			"name": "Авакум",
+			"synonyms": [
+				"Hab"
+			],
+			"numChapters": 3
+		},
+		{
+			"name": "Софония",
+			"synonyms": [
+				"Zeph"
+			],
+			"numChapters": 3
+		},
+		{
+			"name": "Агей",
+			"synonyms": [
+				"Hag"
+			],
+			"numChapters": 2
+		},
+		{
+			"name": "Захария",
+			"synonyms": [
+				"Zech"
+			],
+			"numChapters": 14
+		},
+		{
+			"name": "Малахия",
+			"synonyms": [
+				"Mal"
+			],
+			"numChapters": 4
+		},
+		{
+			"name": "Матей",
+			"synonyms": [
+				"Matt"
+			],
+			"numChapters": 28
+		},
+		{
+			"name": "Марк",
+			"synonyms": [
+				"Mark"
+			],
+			"numChapters": 16
+		},
+		{
+			"name": "Лука",
+			"synonyms": [
+				"Luke"
+			],
+			"numChapters": 24
+		},
+		{
+			"name": "Йоан",
+			"synonyms": [
+				"John"
+			],
+			"numChapters": 21
+		},
+		{
+			"name": "Деянията На Апостолите",
+			"synonyms": [
+				"Acts"
+			],
+			"numChapters": 28
+		},
+		{
+			"name": "Яков",
+			"synonyms": [
+				"Jas"
+			],
+			"numChapters": 5
+		},
+		{
+			"name": "1 Петър",
+			"synonyms": [
+				"1Pet"
+			],
+			"numChapters": 5
+		},
+		{
+			"name": "2 Петър",
+			"synonyms": [
+				"2Pet"
+			],
+			"numChapters": 3
+		},
+		{
+			"name": "1 Йоан",
+			"synonyms": [
+				"1John"
+			],
+			"numChapters": 5
+		},
+		{
+			"name": "2 Йоан",
+			"synonyms": [
+				"2John"
+			],
+			"numChapters": 1
+		},
+		{
+			"name": "3 Йоан",
+			"synonyms": [
+				"3John"
+			],
+			"numChapters": 1
+		},
+		{
+			"name": "Юда",
+			"synonyms": [
+				"Jude"
+			],
+			"numChapters": 1
+		},
+		{
+			"name": "Римляни",
+			"synonyms": [
+				"Rom"
+			],
+			"numChapters": 16
+		},
+		{
+			"name": "1 Коринтяни",
+			"synonyms": [
+				"1Cor"
+			],
+			"numChapters": 16
+		},
+		{
+			"name": "2 Коринтяни",
+			"synonyms": [
+				"2Cor"
+			],
+			"numChapters": 13
+		},
+		{
+			"name": "Галатяни",
+			"synonyms": [
+				"Gal"
+			],
+			"numChapters": 6
+		},
+		{
+			"name": "Ефесяни",
+			"synonyms": [
+				"Eph"
+			],
+			"numChapters": 6
+		},
+		{
+			"name": "Филипяни",
+			"synonyms": [
+				"Phil"
+			],
+			"numChapters": 4
+		},
+		{
+			"name": "Колосяни",
+			"synonyms": [
+				"Col"
+			],
+			"numChapters": 4
+		},
+		{
+			"name": "1 Солунци",
+			"synonyms": [
+				"1Thess"
+			],
+			"numChapters": 5
+		},
+		{
+			"name": "2 Солунци",
+			"synonyms": [
+				"2Thess"
+			],
+			"numChapters": 3
+		},
+		{
+			"name": "1 Тимотей",
+			"synonyms": [
+				"1Tim"
+			],
+			"numChapters": 6
+		},
+		{
+			"name": "2 Тимотей",
+			"synonyms": [
+				"2Tim"
+			],
+			"numChapters": 4
+		},
+		{
+			"name": "Тит",
+			"synonyms": [
+				"Titus"
+			],
+			"numChapters": 3
+		},
+		{
+			"name": "Филимон",
+			"synonyms": [
+				"Phlm"
+			],
+			"numChapters": 1
+		},
+		{
+			"name": "Евреи",
+			"synonyms": [
+				"Heb"
+			],
+			"numChapters": 13
+		},
+		{
+			"name": "Откровението На Йоан",
+			"synonyms": [
+				"Rev"
+			],
+			"numChapters": 22
+		}
+	]
+};
+module.exports = info;
