7e99617908912c588bde91e446c583722b9568f2
VitalikL <lim.vitaliy@gmail.com>
2022-11-04 17:48:49 -0400

    Adding kjp/pwokyn Bible

---
+++ bibles/kjp/pwokyn/info.js
@@ -0,0 +1,469 @@
+var info = {
+	"lang": "kjp",
+	"version": "pwokyn",
+	"books": [
+		{
+			"name": "ၥ မိစ့ၭ",
+			"synonyms": [
+				"Gen"
+			],
+			"numChapters": 50
+		},
+		{
+			"name": "၂ မိစ့ၭ",
+			"synonyms": [
+				"Exod"
+			],
+			"numChapters": 40
+		},
+		{
+			"name": "၃ မိစ့ၭ",
+			"synonyms": [
+				"Lev"
+			],
+			"numChapters": 27
+		},
+		{
+			"name": "၄ မိစ့ၭ",
+			"synonyms": [
+				"Num"
+			],
+			"numChapters": 36
+		},
+		{
+			"name": "၅ မိစ့ၭ",
+			"synonyms": [
+				"Deut"
+			],
+			"numChapters": 34
+		},
+		{
+			"name": "ယီရှုၫအ့ၫ.",
+			"synonyms": [
+				"Josh"
+			],
+			"numChapters": 24
+		},
+		{
+			"name": "စ့ၩရၫ့",
+			"synonyms": [
+				"Judg"
+			],
+			"numChapters": 21
+		},
+		{
+			"name": "ရုၥၭ",
+			"synonyms": [
+				"Ruth"
+			],
+			"numChapters": 4
+		},
+		{
+			"name": "ၥ စၫမုၫအ့လၭ",
+			"synonyms": [
+				"1Sam"
+			],
+			"numChapters": 31
+		},
+		{
+			"name": "၂ စၫမုၫအ့လၭ",
+			"synonyms": [
+				"2Sam"
+			],
+			"numChapters": 24
+		},
+		{
+			"name": "ၥ စဎွၩ",
+			"synonyms": [
+				"1Kgs"
+			],
+			"numChapters": 22
+		},
+		{
+			"name": "၂ စဎွၩ",
+			"synonyms": [
+				"2Kgs"
+			],
+			"numChapters": 25
+		},
+		{
+			"name": "၁ ကွ့ၭနဲၪ့ဆၧ",
+			"synonyms": [
+				"1Chr"
+			],
+			"numChapters": 29
+		},
+		{
+			"name": "၂ ကွ့ၭနဲၪ့ဆၧ",
+			"synonyms": [
+				"2Chr"
+			],
+			"numChapters": 36
+		},
+		{
+			"name": "အ့ဇရၫ",
+			"synonyms": [
+				"Ezra"
+			],
+			"numChapters": 10
+		},
+		{
+			"name": "နံၫဟမဲအၫ",
+			"synonyms": [
+				"Neh"
+			],
+			"numChapters": 13
+		},
+		{
+			"name": "အ့စတၧၫ",
+			"synonyms": [
+				"Esth"
+			],
+			"numChapters": 10
+		},
+		{
+			"name": "ယိဘၭ",
+			"synonyms": [
+				"Job"
+			],
+			"numChapters": 42
+		},
+		{
+			"name": "အၪ့အူၭ",
+			"synonyms": [
+				"Ps"
+			],
+			"numChapters": 150
+		},
+		{
+			"name": "ချဲၩ့ဒီၩ့လၩ့",
+			"synonyms": [
+				"Prov"
+			],
+			"numChapters": 31
+		},
+		{
+			"name": "လီၩလၩ့ဆၧ",
+			"synonyms": [
+				"Eccl"
+			],
+			"numChapters": 12
+		},
+		{
+			"name": "စီလမၧၫ့အထၭခိၪ",
+			"synonyms": [
+				"Song"
+			],
+			"numChapters": 8
+		},
+		{
+			"name": "အဲၫစ့ယၫ",
+			"synonyms": [
+				"Isa"
+			],
+			"numChapters": 66
+		},
+		{
+			"name": "ယ့ၫရမဲအၫ",
+			"synonyms": [
+				"Jer"
+			],
+			"numChapters": 52
+		},
+		{
+			"name": "ဂၪ့ဆၧယၧၩ့ဆၧ",
+			"synonyms": [
+				"Lam"
+			],
+			"numChapters": 5
+		},
+		{
+			"name": "အဇံၫခအ့လၭ",
+			"synonyms": [
+				"Ezek"
+			],
+			"numChapters": 48
+		},
+		{
+			"name": "ဒၫ့နအ့လၭ",
+			"synonyms": [
+				"Dan"
+			],
+			"numChapters": 12
+		},
+		{
+			"name": "ဟိၫစံအၫ",
+			"synonyms": [
+				"Hos"
+			],
+			"numChapters": 14
+		},
+		{
+			"name": "ယိၫအ့လၭ",
+			"synonyms": [
+				"Joel"
+			],
+			"numChapters": 3
+		},
+		{
+			"name": "အၫမူၭ",
+			"synonyms": [
+				"Amos"
+			],
+			"numChapters": 9
+		},
+		{
+			"name": "အိၫဘဒဲအၫ",
+			"synonyms": [
+				"Obad"
+			],
+			"numChapters": 1
+		},
+		{
+			"name": "ယိနၫ",
+			"synonyms": [
+				"Jonah"
+			],
+			"numChapters": 4
+		},
+		{
+			"name": "မဲကၫ",
+			"synonyms": [
+				"Mic"
+			],
+			"numChapters": 7
+		},
+		{
+			"name": "နၫဟၧၫ့",
+			"synonyms": [
+				"Nah"
+			],
+			"numChapters": 3
+		},
+		{
+			"name": "ဟဘၭကၧၭ",
+			"synonyms": [
+				"Hab"
+			],
+			"numChapters": 3
+		},
+		{
+			"name": "ဇ့ၭဖနဲအၫ",
+			"synonyms": [
+				"Zeph"
+			],
+			"numChapters": 3
+		},
+		{
+			"name": "ဟၭဂဲၫ",
+			"synonyms": [
+				"Hag"
+			],
+			"numChapters": 2
+		},
+		{
+			"name": "ဇ့ၭခရဲအၫ",
+			"synonyms": [
+				"Zech"
+			],
+			"numChapters": 14
+		},
+		{
+			"name": "မၪလခဲၫ",
+			"synonyms": [
+				"Mal"
+			],
+			"numChapters": 4
+		},
+		{
+			"name": "မၭၥုၫ",
+			"synonyms": [
+				"Matt"
+			],
+			"numChapters": 28
+		},
+		{
+			"name": "မၫကၧၭ",
+			"synonyms": [
+				"Mark"
+			],
+			"numChapters": 16
+		},
+		{
+			"name": "လုကၭ",
+			"synonyms": [
+				"Luke"
+			],
+			"numChapters": 24
+		},
+		{
+			"name": "ယီဟၫ့",
+			"synonyms": [
+				"John"
+			],
+			"numChapters": 21
+		},
+		{
+			"name": "ၦဆၧ",
+			"synonyms": [
+				"Acts"
+			],
+			"numChapters": 28
+		},
+		{
+			"name": "ရိမၫ",
+			"synonyms": [
+				"Rom"
+			],
+			"numChapters": 16
+		},
+		{
+			"name": "၁ ကီရံၫ့",
+			"synonyms": [
+				"1Cor"
+			],
+			"numChapters": 16
+		},
+		{
+			"name": "၂ ကီရံၫ့",
+			"synonyms": [
+				"2Cor"
+			],
+			"numChapters": 13
+		},
+		{
+			"name": "ဂလ့ၡၫ",
+			"synonyms": [
+				"Gal"
+			],
+			"numChapters": 6
+		},
+		{
+			"name": "အ့ဖစၧၭ",
+			"synonyms": [
+				"Eph"
+			],
+			"numChapters": 6
+		},
+		{
+			"name": "ဖလံဖံၫ",
+			"synonyms": [
+				"Phil"
+			],
+			"numChapters": 4
+		},
+		{
+			"name": "ကလီစံၫ",
+			"synonyms": [
+				"Col"
+			],
+			"numChapters": 4
+		},
+		{
+			"name": "၁ ၥ့ၭစလနဲကၫ",
+			"synonyms": [
+				"1Thess"
+			],
+			"numChapters": 5
+		},
+		{
+			"name": "၂ ၥ့ၭစလနဲကၫ",
+			"synonyms": [
+				"2Thess"
+			],
+			"numChapters": 3
+		},
+		{
+			"name": "၁ ထံမၥံၫ",
+			"synonyms": [
+				"1Tim"
+			],
+			"numChapters": 6
+		},
+		{
+			"name": "၂ ထံမၥံၫ",
+			"synonyms": [
+				"2Tim"
+			],
+			"numChapters": 4
+		},
+		{
+			"name": "တဲတၧၭ",
+			"synonyms": [
+				"Titus"
+			],
+			"numChapters": 3
+		},
+		{
+			"name": "ဖလံမၧၫ့",
+			"synonyms": [
+				"Phlm"
+			],
+			"numChapters": 1
+		},
+		{
+			"name": "ဟံဘြႃၫ",
+			"synonyms": [
+				"Heb"
+			],
+			"numChapters": 13
+		},
+		{
+			"name": "ယ့မံၭ",
+			"synonyms": [
+				"Jas"
+			],
+			"numChapters": 5
+		},
+		{
+			"name": "၁ ပံတၧၫ",
+			"synonyms": [
+				"1Pet"
+			],
+			"numChapters": 5
+		},
+		{
+			"name": "၂ ပံတၧၫ",
+			"synonyms": [
+				"2Pet"
+			],
+			"numChapters": 3
+		},
+		{
+			"name": "၁ ယီဟၫ့",
+			"synonyms": [
+				"1John"
+			],
+			"numChapters": 5
+		},
+		{
+			"name": "၂ ယီဟၫ့",
+			"synonyms": [
+				"2John"
+			],
+			"numChapters": 1
+		},
+		{
+			"name": "၃ ယီဟၫ့",
+			"synonyms": [
+				"3John"
+			],
+			"numChapters": 1
+		},
+		{
+			"name": "ယုဒၭ",
+			"synonyms": [
+				"Jude"
+			],
+			"numChapters": 1
+		},
+		{
+			"name": "နဲၪဖျီၪ",
+			"synonyms": [
+				"Rev"
+			],
+			"numChapters": 22
+		}
+	]
+};
+module.exports = info;
